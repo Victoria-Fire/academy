cb8e26e4a69291f5166771599ac02f1913d9c484
Victoria-Fire <vik.fire@yandex.ru>
2021-10-27 07:45:05 +0300

    Updated Dog

--- academy/src/main/java/by/academy/classwork/lesson2/Dog.java
+++ academy/src/main/java/by/academy/classwork/lesson2/Dog.java
@@ -5,14 +5,4 @@ public class Dog {
 	public void bark( ) {
 		System.out.println("Соседская собака гавкает на кота!!!");
 	}
-
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("Dog []");
-		return builder.toString();
-	}
-	
-	
-
 }
