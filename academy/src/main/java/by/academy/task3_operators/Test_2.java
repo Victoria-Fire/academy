a0c28f412c782f881fe813a829c754f2e8ed21f9
Victoria-Fire <vik.fire@yandex.ru>
2021-10-29 23:12:53 +0300

    Added Test_2

--- academy/src/main/java/by/academy/task3_operators/Test_2.java
+++ academy/src/main/java/by/academy/task3_operators/Test_2.java
@@ -3,8 +3,9 @@ package by.academy.task3_operators;
 public class Test_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 2 + 1;
+		int j = 3 + 1;
+		int k = i * i + j * j;
+		System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
 	}
-
 }
