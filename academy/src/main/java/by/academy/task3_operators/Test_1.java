b30b626f381d7a8fcf6e5d70a4800bbe1e8eb344
Victoria-Fire <vik.fire@yandex.ru>
2021-10-29 22:58:14 +0300

    Added Test_1

--- academy/src/main/java/by/academy/task3_operators/Test_1.java
+++ academy/src/main/java/by/academy/task3_operators/Test_1.java
@@ -3,7 +3,16 @@ package by.academy.task3_operators;
 public class Test_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 2_123_000;
+		int b = 100;
+		int c = 234;
+		int result = a / b * c;
+		System.out.println("Ответ: "+ result);
+		System.out.println();
+		String textResult = Integer.toString(result);
+		String myResult = "Красивый ответ: " + textResult.substring(0, 1) + "_" + textResult.substring(1, 4) + "_" + textResult.substring(4, 7);
+		
+		System.out.println(myResult);
 
 	}
 
