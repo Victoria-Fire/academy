f6278606d3bf6979422738f9e592b75653fbbeda
Victoria-Fire <vik.fire@yandex.ru>
2021-10-30 00:19:40 +0300

    Added Test_4

--- academy/src/main/java/by/academy/task3_operators/Test_4.java
+++ academy/src/main/java/by/academy/task3_operators/Test_4.java
@@ -3,7 +3,35 @@ package by.academy.task3_operators;
 public class Test_4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int s = 370_000_000;
+		int sec;
+		int m;
+		int min;
+		int h;
+		int hour;
+		int d;
+		int day;
+		int w;
+		int week;
+		int mo;
+		int month;
+		int year;
+
+		sec = s % 60;
+		m = (s - sec) / 60;
+		min = m % 60;
+		h = (m - min) / 60;
+		hour = h % 24;
+		d = (h - hour) / 24;
+		day = d % 7;
+		w = (d - day) / 7;
+		week = w % 4;
+		mo = (w - week) / 4;
+		month = mo % 12;
+		year = (mo - month) / 12;
+		 
+		System.out.println(year + " лет " + month + " месяцев " + week + " недель " + day + " дней " 
+		+ hour + " часов " + min + " минут " + sec + " секунд");
 
 	}
 
