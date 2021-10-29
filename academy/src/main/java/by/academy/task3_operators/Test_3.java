076f56ae02ddb560468d36ae6e0a3d99a8123d8a
Victoria-Fire <vik.fire@yandex.ru>
2021-10-29 23:25:20 +0300

    Added Test_3

--- academy/src/main/java/by/academy/task3_operators/Test_3.java
+++ academy/src/main/java/by/academy/task3_operators/Test_3.java
@@ -1,9 +1,19 @@
 package by.academy.task3_operators;
 
+import java.util.Scanner;
+
 public class Test_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите число:");
+		int number = sc.nextInt();
+		
+		if(number % 2 == 0) {
+			System.out.println("Ваше число " + number + " - чётное =)");
+		} else {
+			System.out.println("Ваше число " + number + " - нечётное =(");
+		}
 
 	}
 
