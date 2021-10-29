15cdcadf058e80323605f63af2b1e21bf05c90dd
Victoria-Fire <vik.fire@yandex.ru>
2021-10-30 01:10:14 +0300

    Added Test_5

--- academy/src/main/java/by/academy/task3_operators/Test_5.java
+++ academy/src/main/java/by/academy/task3_operators/Test_5.java
@@ -1,9 +1,28 @@
 package by.academy.task3_operators;
 
+import java.util.Scanner;
+
 public class Test_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите число a:");
+		int a = sc.nextInt();
+		System.out.println("Введите число b:");
+		int b = sc.nextInt();
+		double c = (double) (a + b) / 2;
+		
+		if(a > b) {
+			System.out.println("Число " + a + " больше числа " + b);
+		} else if(a < b) {
+			System.out.println("Число " + a + " меньше числа " + b);
+		} else {
+			System.out.println("Опаньки! Наши числа равны)");
+		}
+		
+		System.out.println("Среднее арифметическое равно: " + c);
+				
+		sc.close();	
 
 	}
 
