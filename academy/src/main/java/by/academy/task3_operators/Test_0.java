9dbcf59e9dc278425db66005168d1e7c45d8e021
Victoria-Fire <vik.fire@yandex.ru>
2021-10-29 22:39:40 +0300

    Added Test_0

--- academy/src/main/java/by/academy/task3_operators/Test_0.java
+++ academy/src/main/java/by/academy/task3_operators/Test_0.java
@@ -1,22 +1,56 @@
 package by.academy.task3_operators;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Test_0 {
 
-//	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		String month = sc.nextLine();
-//		
-//		switch(month) { //ругается на String
-//		case "январь":
-//			System.out.println(1);
-//			break;
-//		case "февраль":
-//			System.out.println(2);
-//			break;
-//		}
-//
-//	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Введите любой месяц:");
+		String month = sc.nextLine();
+
+		switch (month) {
+		case "Январь":
+			System.out.println("Ваш месяц под номером - " + 1);
+			break;
+		case "Февраль":
+			System.out.println("Ваш месяц под номером - " + 2);
+			break;
+		case "Март":
+			System.out.println("Ваш месяц под номером - " + 3);
+			break;
+		case "Апрель":
+			System.out.println("Ваш месяц под номером - " + 4);
+			break;
+		case "Май":
+			System.out.println("Ваш месяц под номером - " + 5);
+			break;
+		case "Июнь":
+			System.out.println("Ваш месяц под номером - " + 6);
+			break;
+		case "Июль":
+			System.out.println("Ваш месяц под номером - " + 7);
+			break;
+		case "Август":
+			System.out.println("Ваш месяц под номером - " + 8);
+			break;
+		case "Сентябрь":
+			System.out.println("Ваш месяц под номером - " + 9);
+			break;
+		case "Октябрь":
+			System.out.println("Ваш месяц под номером - " + 10);
+			break;
+		case "Ноябрь":
+			System.out.println("Ваш месяц под номером - " + 11);
+			break;
+		case "Декабрь":
+			System.out.println("Ваш месяц под номером - " + 12);
+			break;
+		default:
+			System.out.println("Упс! Такого месяца не существует =)");
+
+		}
+
+	}
 
 }
