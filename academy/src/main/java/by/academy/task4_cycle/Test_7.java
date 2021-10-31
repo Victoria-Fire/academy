76478bd6d063348747c2575a1f1b063746a7c2e1
Victoria-Fire <vik.fire@yandex.ru>
2021-10-31 18:18:49 +0300

    Test_7 has been done

---
+++ academy/src/main/java/by/academy/task4_cycle/Test_7.java
@@ -0,0 +1,41 @@
+package by.academy.task4_cycle;
+
+import java.util.Scanner;
+
+public class Test_7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] array1 = new int[10];
+		int[] array2 = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Введите число а:");
+			array1[i] = sc.nextInt();
+			System.out.println("Введите число b:");
+			array2[i] = sc.nextInt();
+			System.out.println("_________________");
+
+		}
+		for (int i = 0; i < 10; i++) {
+			if (array1[i] > array2[i]) {
+				System.out.println("Сравниваем числа " + array1[i] + " и " + array2[i]);
+				System.out.println("Число " + array1[i] + " большее в этой паре");
+				System.out.println();
+			} else if (array1[i] < array2[i]) {
+				System.out.println("Сравниваем числа " + array1[i] + " и " + array2[i]);
+				System.out.println("Число " + array2[i] + " большее в этой паре");
+				System.out.println();
+			} else {
+				System.out.println("Сравниваем числа " + array1[i] + " и " + array2[i]);
+				System.out.println("Числа равны!");
+				System.out.println();
+			}
+		}
+
+		sc.close();
+	}
+
+}
+
+//Ввести с клавиатуры 10 пар чисел. сравнить числа в каждой паре и напечатать большие из них.?
