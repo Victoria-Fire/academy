c8d1f66cdfd3c68d60a69f240c431fec753a486f
Victoria-Fire <vik.fire@yandex.ru>
2021-11-02 20:30:18 +0300

    Test_0 has been added

---
+++ academy/src/main/java/by/academy/task4_cycle_classwork/Test_0.java
@@ -0,0 +1,13 @@
+package by.academy.task4_cycle_classwork;
+
+public class Test_0 {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0].substring(0, 1));
+		if(a % 2 != 0) {
+			System.out.println("Мое число: " + a);
+		}
+
+	}
+
+}
