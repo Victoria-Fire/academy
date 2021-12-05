f461d0af10390c697798563cb5d06440f4a95507
Victoria-Fire <vik.fire@yandex.ru>
2021-12-05 12:50:55 +0300

    lesson13 has been added (not finished)

---
+++ academy/src/main/java/by/academy/classwork/lesson13/CustomLinkedList.java
@@ -0,0 +1,235 @@
+package by.academy.classwork.lesson13;
+
+public class CustomLinkedList<T> {
+
+	private Node last;
+	private Node first;
+	private int size;
+
+	public int size() {
+		return size;
+	}
+
+	public void add(T value) {
+		Node next = null;
+		Node prev = last;
+
+		Node newNode = new Node(next, prev, value);
+
+		if (prev == null) {
+			last = newNode;
+			first = newNode;
+		} else {
+			last.next = newNode;
+			newNode.prev = last;
+			last = newNode;
+		}
+		size++;
+	}
+
+	public void add(int index, T value) {
+		if (size < index) {
+			System.out.println("Ошибка");
+			return;
+		} else if (size == index) {
+			add(value);
+			return;
+		} else {
+			Node node = new Node(value);
+			Integer counter = 0;
+			Node current = first;
+			while (counter < index) {
+				current = current.next;
+				counter++;
+			}
+			node.next = current;
+			node.prev = current.prev;
+			current.prev.next = node;
+			current.prev = node;
+		}
+	}
+
+	public Node getNode(int index) {
+		if (index >= size) {
+			System.err.println("index больше size");
+			return null;
+		}
+		int counter = 0;
+		Node current = first;
+		while (current != null && counter <= index) {
+			if (index == counter) {
+				return current;
+			}
+			current = current.next;
+			counter++;
+		}
+		return null;
+	}
+
+//	private Node<T> getNode(int index) {
+//        int counter = 0;
+//        Node<T> node = head;
+//        while (node != null) {
+//            if (counter == index) {
+//                break;
+//            }
+//            node = node.next;
+//            counter++;
+//        }
+//        return node;
+//    }
+
+//	 private Node<T> getNode(int index) {
+//         int counter = 0;
+//         Node<T> node = head;
+//         while (node != null) {
+//             if (counter == index) {
+//                 break;
+//             }
+//             node = node.next;
+//             counter++;
+//         }
+//         return node;
+//     }
+
+//	public T get(int index) {
+//        Node<T> node = head;
+//        int current = 0;
+//        while (current < index) {
+//            node = node.next;
+//            current++;
+//        }
+//        return node.value;
+//    }
+
+
+//	public Node getNode(T object) {
+//		Node current = first;
+//		while (current != null && counter <= index) {
+//			if (index == counter) {
+//				return current;
+//			}
+//			current = current.next;
+//			counter++;
+//		}
+//		return null;
+//	}
+
+	public T get(int index) {
+		return getNode(index).value;
+	}
+
+//	public T get(int index) {
+//        int counter = 0;
+//        Node<T> node = head;
+//        while (node != null) {
+//            if (counter == index) {
+//                break;
+//            }
+//            node = node.next;
+//            counter++;
+//        }
+//        return node.value;
+//    }
+
+	public void get(T object) {
+
+	}
+
+	public T remove(int index) {
+		Node current = last;
+
+		return null;
+
+	}
+
+//    public void remove(int index) {
+//        if (size == 1 && index == 0) {
+//            head = null;
+//            tail = null;
+//            size = 0;
+//            return;
+//        }
+//        if (index == size - 1) {
+//            Node<T> prev = tail.prev;
+//            prev.next = null;
+//            tail = prev;
+//            size--;
+//            return;
+//        }
+//        if (index >= 0 && index < size) {
+//            Node<T> node = getNode(index);
+//            Node<T> prev = node.prev;
+//            Node<T> next = node.next;
+//            if (index == 0) {
+//                head = next;
+//            }
+//            if (prev != null) {
+//                prev.next = next;
+//            }
+//            next.prev = prev;
+//            size--;
+//            return;
+//        }
+//        System.out.println("Index of bound");
+//    }
+
+	public void remove(T object) {
+
+	}
+
+//	public T getTailValue() {
+//        if (tail != null) {
+//            return tail.value;
+//        }
+//        return null;
+//    }
+
+//	public T getHeadValue() {
+//        if (head != null) {
+//            return head.value;
+//        }
+//        return null;
+//    }
+	
+	 public void print() {
+         Node node = first;
+         while (node != null) {
+             System.out.println(node.value);
+             node = node.next;
+         }
+         System.out.println();
+     }
+
+	class Node {
+		Node next;
+		Node prev;
+		T value;
+
+		Node(T value) {
+			super();
+			this.value = value;
+		}
+
+		Node(Node prev, Node next, T value) {
+			super();
+			this.prev = prev;
+			this.next = next;
+			this.value = value;
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("Node [next=");
+			builder.append(next);
+			builder.append(", prev=");
+			builder.append(prev);
+			builder.append(", value=");
+			builder.append(value);
+			builder.append("]");
+			return builder.toString();
+		}
+
+	}
+}
---
+++ academy/src/main/java/by/academy/classwork/lesson13/CustomLinkedListDemo.java
@@ -0,0 +1,24 @@
+package by.academy.classwork.lesson13;
+
+public class CustomLinkedListDemo {
+
+	public static void main(String... args) {
+		CustomLinkedList<Integer> list = new CustomLinkedList<>();
+		list.add(1);
+		list.add(2);
+//		list.add(3);
+//		list.add(4);
+		list.print();
+//		list.add(1, 14);
+//		list.print();
+//		list.remove(3);
+//		list.print();
+//		list.remove(0);
+//		list.print();
+//		System.out.println(custom.get(2));
+//		System.out.println(list.getHeadValue());
+//		System.out.println(list.getTailValue());
+//		
+	}
+
+}
